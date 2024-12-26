4e1c6c1d59b6227f735b761307563ea304e43b0d
寒焰 <hanyanxx1@163.com>
2024-12-26 10:37:32 +0800

    一些流程图

--- src/react-transition-group/Transition.ts
+++ src/react-transition-group/Transition.ts
@@ -6,11 +6,13 @@ export const ENTERED = "entered"; //进入后
 export const EXITING = "exiting"; //退出中
 export const EXITED = "exited"; //退出后
 
+// Transition会自动帮我们管理过渡状态(entering, entered, exiting, exited)
 class Transition extends React.Component {
   static contextType = TransitionGroupContext;
 
   constructor(props: any) {
     super(props);
+    //我们可以向Transition传递in和timeout属性
     this.state = {
       status: this.props.in ? ENTERED : EXITED,
     };
@@ -26,6 +28,7 @@ class Transition extends React.Component {
   }
 
   componentDidUpdate() {
+    //通过in来控制组件是否显示
     let { status } = this.state;
     //更新后当属性发生改变时更改状态
     if (this.props.in) {
@@ -42,6 +45,7 @@ class Transition extends React.Component {
   }
 
   onTransitionEnd(timeout, callback) {
+    //通过timeout来控制显示或消失的时间间隔
     if (timeout) {
       setTimeout(callback, timeout);
     }
@@ -84,7 +88,12 @@ class Transition extends React.Component {
   render() {
     const { children } = this.props;
     const { status } = this.state;
-    return typeof children === "function" ? children(status) : children;
+    return typeof children === "function"
+      ? // Transition组件的children是一个函数，当状态发生改变时，
+        // 会把新的状态传递给children函数参数，
+        // 从而可以根据不同的状态渲染不同的样式
+        children(status)
+      : children;
   }
 }
 
